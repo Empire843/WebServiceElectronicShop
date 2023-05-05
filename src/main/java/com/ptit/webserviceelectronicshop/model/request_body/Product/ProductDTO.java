675e6028fa7fa38f77e5f4e500b4dac03516937d
Empire <56126766+Empire843@users.noreply.github.com>
2023-05-05 16:22:45 +0700

    Update ProductDTO.java

--- src/main/java/com/ptit/webserviceelectronicshop/model/request_body/Product/ProductDTO.java
+++ src/main/java/com/ptit/webserviceelectronicshop/model/request_body/Product/ProductDTO.java
@@ -6,11 +6,11 @@ import java.time.LocalDateTime;
 
 @Data
 public class ProductDTO {
+
     private Long category_id;
     private String name;
     private Double price;
     private String description;
-    private LocalDateTime created_date;
     private int quantity;
     private String image;
 }
